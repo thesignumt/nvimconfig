78a83cc2242412daf208f1667a6b3b2cef045329
sigmacodeslol <m96153045@gmail.com>
2025-08-10 23:58:00 +0100

    the plugin is kinda trashed bc it has a bug and i am too lazy to fix lmao

--- lua/custom/plugins/Cmtsep.lua
+++ lua/custom/plugins/Cmtsep.lua
@@ -1,6 +1,12 @@
 return {
   'sigmacodeslol/cmtsep.nvim',
   callback = function()
-    require('cmtsep').setup { preset = 'sigmacodeslol', key = ';;' }
+    local cmtsep = require 'cmtsep'
+    cmtsep.setup { preset = 'sigmacodeslol' }
+    vim.keymap.set('n', ';;', cmtsep.insert_comment_block, {
+      noremap = true,
+      silent = true,
+      desc = 'Insert beautified comment block separator',
+    })
   end,
 }
