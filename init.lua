63e136a201a5c2e8d1f50e6628b02bc633737f59
sigmacodeslol <m96153045@gmail.com>
2025-08-10 20:36:11 +0100

    transfered luasnip to my main init.lua

--- init.lua
+++ init.lua
@@ -905,29 +905,27 @@ require('lazy').setup({
     event = 'InsertEnter',
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
-      -- {
-      --   'L3MON4D3/LuaSnip',
-      --   build = (function()
-      --     -- Build Step is needed for regex support in snippets.
-      --     -- This step is not supported in many windows environments.
-      --     -- Remove the below condition to re-enable on windows.
-      --     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-      --       return
-      --     end
-      --     return 'make install_jsregexp'
-      --   end)(),
-      --   dependencies = {
-      --     -- `friendly-snippets` contains a variety of premade snippets.
-      --     --    See the README about individual language/framework/plugin snippets:
-      --     --    https://github.com/rafamadriz/friendly-snippets
-      --     -- {
-      --     --   'rafamadriz/friendly-snippets',
-      --     --   config = function()
-      --     --     require('luasnip.loaders.from_vscode').lazy_load()
-      --     --   end,
-      --     -- },
-      --   },
-      -- },
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          -- NOTE: Removed the condition
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+      },
       'saadparwaiz1/cmp_luasnip',
 
       -- Adds other completion capabilities.
