6b017d5a23214a1df74d60a78277993f7337221d
sigmacodeslol <m96153045@gmail.com>
2025-08-11 19:47:57 +0100

    more snippets

---
+++ snippets/all.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: undefined-global
+
+return {
+  s('date', t(os.date '%Y/%m/%d')),
+  s('mail', t 'tcrrxzz@gmail.com'),
+  s('email', t 'tcrrxzz@gmail.com'),
+  s('gh', t 'github.com/sigmacodeslol'),
+}
---
+++ snippets/python.lua
@@ -0,0 +1,11 @@
+---@diagnostic disable: undefined-global
+
+return {
+  s('ifmain', {
+    t {
+      'if __name__ == "__main__":',
+      '\t',
+    },
+    i(1),
+  }),
+}
