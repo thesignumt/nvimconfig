149b65e4341e99c25799ea7bb681627934d84ca3
sigmacodeslol <m96153045@gmail.com>
2025-08-12 18:11:24 +0100

    code companion -> copilot.vim
    code companion wasn't working at all (i was using copilot) and the other
    ai's either need api keys or take up a bunch of space to store

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -1,8 +1,1 @@
-return {
-  'olimorris/codecompanion.nvim',
-  opts = {},
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
-  },
-}
+return { 'github/copilot.vim' }
