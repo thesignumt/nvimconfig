969afb4bd12ab1917ef485d0d3bc54cb314c0054
sigmacodeslol <m96153045@gmail.com>
2025-08-13 17:28:24 +0100

    codecompanion

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -1,1 +1,11 @@
-return { 'github/copilot.vim' }
+return {
+  { 'github/copilot.vim' },
+  {
+    'olimorris/codecompanion.nvim',
+    opts = {},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+}
