8eebf25841506b6d28929c2339a2a3584e95f96a
sigmacodeslol <m96153045@gmail.com>
2025-09-02 19:48:17 +0100

    ive been trying to get ruff to know that i am using python3.13.6

--- plugin/options.lua
+++ plugin/options.lua
@@ -2,11 +2,17 @@
 -- LSP Servers Setup
 ------------------------------------------------------------
 
-local lspservers = {
-  'clangd',
-  'pyright',
+local lspconfig = require 'lspconfig'
+
+lspconfig.clangd.setup {}
+
+lspconfig.pyright.setup {
+  settings = {
+    python = {
+      pythonPath = 'C:\\Python313\\python.exe',
+    },
+  },
 }
-vim.lsp.enable(lspservers)
 
 ------------------------------------------------------------
 -- General Vim Options
