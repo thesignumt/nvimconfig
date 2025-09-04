28c971bda36c4dc947cac5210842d98a663a57f4
sigmacodeslol <m96153045@gmail.com>
2025-09-04 08:37:12 +0100

    using harpoon instead of buffer to not waste time
    i am just trying it out since i haven't really mastered harpoon yet

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,23 +1,36 @@
 return {
-  "ThePrimeagen/harpoon",
+  'ThePrimeagen/harpoon',
   config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    local mark = require 'harpoon.mark'
+    local ui = require 'harpoon.ui'
 
-    vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Mark File" })
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
+    vim.keymap.set(
+      'n',
+      '<leader>a',
+      mark.add_file,
+      { desc = 'Harpoon: Mark File' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>h',
+      ui.toggle_quick_menu,
+      { desc = 'Toggle Harpoon Menu' }
+    )
 
-    vim.keymap.set("n", "<C-t>", function()
+    vim.keymap.set('n', '<leader>1', function()
       ui.nav_file(1)
-    end, { desc = "Harpoon File 1" })
-    vim.keymap.set("n", "<C-s>", function()
+    end, { desc = 'Harpoon File 1' })
+    vim.keymap.set('n', '<leader>2', function()
       ui.nav_file(2)
-    end, { desc = "Harpoon File 2" })
-    vim.keymap.set("n", "<C-b>", function()
+    end, { desc = 'Harpoon File 2' })
+    vim.keymap.set('n', '<leader>3', function()
       ui.nav_file(3)
-    end, { desc = "Harpoon File 3" })
-    vim.keymap.set("n", "<C-g>", function()
+    end, { desc = 'Harpoon File 3' })
+    vim.keymap.set('n', '<leader>4', function()
       ui.nav_file(4)
-    end, { desc = "Harpoon File 4" })
+    end, { desc = 'Harpoon File 4' })
+    vim.keymap.set('n', '<leader>5', function()
+      ui.nav_file(5)
+    end, { desc = 'Harpoon File 5' })
   end,
 }
