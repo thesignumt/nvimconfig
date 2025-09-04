ad4224a67fd8f315d3c1bc143974a94759b11911
sigmacodeslol <m96153045@gmail.com>
2025-09-04 10:52:06 +0100

    feat(harpoon): changed nav keys and improved with loop

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,20 +9,11 @@ return {
     nmap('<leader>a', mark.add_file, 'Harpoon: Mark File')
     nmap('<leader>h', ui.toggle_quick_menu, 'Toggle Harpoon Menu')
 
-    nmap('<leader>1', function()
-      ui.nav_file(1)
-    end, 'Harpoon File 1')
-    nmap('<leader>2', function()
-      ui.nav_file(2)
-    end, 'Harpoon File 2')
-    nmap('<leader>3', function()
-      ui.nav_file(3)
-    end, 'Harpoon File 3')
-    nmap('<leader>4', function()
-      ui.nav_file(4)
-    end, 'Harpoon File 4')
-    nmap('<leader>5', function()
-      ui.nav_file(5)
-    end, 'Harpoon File 5')
+    local nav_keys = { h = 1, j = 2, k = 3, l = 4, [';'] = 5 }
+    for key, idx in pairs(nav_keys) do
+      nmap('g' .. key, function()
+        ui.nav_file(idx)
+      end, 'Harpoon File ' .. idx)
+    end
   end,
 }
