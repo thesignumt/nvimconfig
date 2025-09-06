5a50ee57957c96218c25bd41259be6ba3604ce23
sigmacodeslol <m96153045@gmail.com>
2025-09-06 21:49:21 +0100

    refactor: rearranging code

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -4,9 +4,9 @@ require 'core.genor'
 require 'core.snippets'
 require 'core.typst'
 
+local dblL = '<leader><leader>'
 local m = require 'utils.map'
 local nmap = m.nmap
-local dblL = '<leader><leader>'
 local mkcmt = require 'utils.mkcmt'
 
 mkcmt.setup {}
