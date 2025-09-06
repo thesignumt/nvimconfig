f7d0e8dec0eb509cc911a19dc30d1819e193e7b2
sigmacodeslol <m96153045@gmail.com>
2025-09-06 21:42:59 +0100

    chore(core): required core files

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,9 @@
+require 'core.remaps'
+require 'core.cmds'
+require 'core.genor'
+require 'core.snippets'
+require 'core.typst'
+
 local m = require 'utils.map'
 local nmap = m.nmap
 local dblL = '<leader><leader>'
