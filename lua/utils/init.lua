121c0079d280e24e0c2f47344047546b661b6cbf
sigmacodeslol <m96153045@gmail.com>
2025-09-08 17:03:37 +0100

    chore(utils): to avoid the confusion

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,36 +1,35 @@
 local M = {}
 
-M.fn = {
-  ---make a lazy plugin spec for my plugins
-  ---@param name string name of plugin (without .nvim) e.g. mkcmt.nvim -> mkcmt
-  ---@param opts table
-  ---@return table
-  miplugin = function(name, opts)
-    return vim.tbl_extend(
-      'force',
-      vim.deepcopy {
-        dir = ('%s%s%s'):format(
-          'C:\\justcode\\alpha\\plugins\\',
-          name,
-          '.nvim\\'
-        ),
-        name = name,
-      },
-      opts
-    )
-  end,
-  toggle_comment = function()
-    local line = vim.api.nvim_get_current_line()
-    local comment_string = vim.bo.commentstring:match '^(.*)%%s' or '//'
+---make a lazy plugin spec for my plugins
+---@param name string name of plugin (without .nvim) e.g. mkcmt.nvim -> mkcmt
+---@param opts table
+---@return table
+function M.miplugin(name, opts)
+  return vim.tbl_extend(
+    'force',
+    vim.deepcopy {
+      dir = ('%s%s%s'):format(
+        'C:\\justcode\\alpha\\plugins\\',
+        name,
+        '.nvim\\'
+      ),
+      name = name,
+    },
+    opts
+  )
+end
 
-    if line:match('^%s*' .. vim.pesc(comment_string)) then
-      line = line:gsub('^%s*' .. vim.pesc(comment_string) .. '%s?', '')
-    else
-      line = comment_string .. ' ' .. line
-    end
+function M.toggle_comment()
+  local line = vim.api.nvim_get_current_line()
+  local comment_string = vim.bo.commentstring:match '^(.*)%%s' or '//'
 
-    vim.api.nvim_set_current_line(line)
-  end,
-}
+  if line:match('^%s*' .. vim.pesc(comment_string)) then
+    line = line:gsub('^%s*' .. vim.pesc(comment_string) .. '%s?', '')
+  else
+    line = comment_string .. ' ' .. line
+  end
+
+  vim.api.nvim_set_current_line(line)
+end
 
 return M
