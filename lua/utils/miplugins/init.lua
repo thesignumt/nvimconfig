68e02a983b3c0f857aada0efcdcf7074ef3dbb0c
sigmacodeslol <m96153045@gmail.com>
2025-09-08 08:20:59 +0100

    feat(mkcmt): added after and before text insert

--- lua/utils/miplugins/init.lua
+++ lua/utils/miplugins/init.lua
@@ -3,10 +3,20 @@ return {
   fns.miplugin('mkcmt', {
     opts = {},
     config = function(_, opts)
-      require('mkcmt').setup(opts)
+      local mkcmt = require 'mkcmt'
+      mkcmt.setup(opts)
 
       local nmap = require('utils.map').nmap
-      nmap('<leader><leader>c', require('mkcmt').comment)
+      local after = function()
+        mkcmt.comment { after = true }
+      end
+      local back = function()
+        mkcmt.comment { after = false }
+      end
+
+      local dblL = '<leader><leader>'
+      nmap(dblL .. 'c', after)
+      nmap(dblL .. 'C', back)
     end,
   }),
 }
