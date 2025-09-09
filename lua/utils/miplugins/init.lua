45fd7fb540e8fb1e524fe5c9807c918b21c2aa39
sigmacodeslol <m96153045@gmail.com>
2025-09-09 17:59:26 +0100

    feat(mkcmt): upper cmt blocks

--- lua/utils/miplugins/init.lua
+++ lua/utils/miplugins/init.lua
@@ -9,9 +9,15 @@ return {
     config = function(_, opts)
       local mkcmt = require 'mkcmt'
       mkcmt.setup(opts)
+      local c = { after = true, upper = false }
+      local C = { after = false, upper = false }
+      local x = { after = true, upper = true }
+      local X = { after = false, upper = true }
 
-      ms('nv', m.dblL 'c', fn(mkcmt.comment, { after = true }))
-      ms('nv', m.dblL 'C', fn(mkcmt.comment, { after = false }))
+      ms('nv', m.dblL 'c', fn(mkcmt.comment, c), 'mkcmt after')
+      ms('nv', m.dblL 'C', fn(mkcmt.comment, C), 'mkcmt before')
+      ms('nv', m.dblL 'x', fn(mkcmt.comment, x), 'MKCMT after')
+      ms('nv', m.dblL 'X', fn(mkcmt.comment, X), 'MKCMT before')
     end,
   }),
 }
