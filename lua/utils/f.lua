b0c993acd07afb685f23afe78058678aea54d6c1
sigmacodeslol <m96153045@gmail.com>
2025-09-10 22:15:19 +0100

    chore(f): fns function

--- lua/utils/f.lua
+++ lua/utils/f.lua
@@ -19,4 +19,28 @@ M.fn = function(f, ...)
   end
 end
 
+-- Multiple function creators
+---@param ... table
+---@return function
+M.fns = function(...)
+  local tables = { ... }
+
+  -- Prepare all functions
+  local funcs = {}
+  for _, tbl in ipairs(tables) do
+    local creator = tbl[1] -- first element is the function creator
+    local args = { table.unpack(tbl, 2) } -- rest are arguments
+    table.insert(funcs, creator(unpack(args)))
+  end
+
+  -- Return a function that calls them all
+  return function(...)
+    local results = {}
+    for i, f in ipairs(funcs) do
+      results[i] = f(...)
+    end
+    return unpack(results)
+  end
+end
+
 return M
