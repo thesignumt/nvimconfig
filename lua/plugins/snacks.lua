f97f328425e5b162d551d7842d1f82043379a967
sigmacodeslol <m96153045@gmail.com>
2025-09-14 16:07:07 +0100

    chore: disable dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,7 +8,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = { enabled = false },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
