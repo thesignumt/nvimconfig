10b855cce904f3497f6ec7c9c521356a8831d3c4
thesignumt <m96153045@gmail.com>
2025-09-15 00:29:16 +0100

    feat(dial.nvim): use ga and gs instead of <c-a> and <c-x>

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -1,22 +1,23 @@
 -- Better increase/descrease
 return {
-		"monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-    },
-		config = function()
-			local augend = require("dial.augend")
-			require("dial.config").augends:register_group({
-				default = {
-					augend.integer.alias.decimal,
-					augend.integer.alias.hex,
-					augend.date.alias["%Y/%m/%d"],
-					augend.constant.alias.bool,
-					augend.semver.alias.semver,
-					augend.constant.new({ elements = { "let", "const" } }),
-				},
-			})
-        end,
+  'monaqa/dial.nvim',
+  keys = {},
+  config = function()
+    local augend = require 'dial.augend'
+    require('dial.config').augends:register_group {
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias['%Y/%m/%d'],
+        augend.constant.alias.bool,
+        augend.semver.alias.semver,
+        augend.constant.new { elements = { 'let', 'const' } },
+      },
+    }
+
+    local dialmap = require 'dial.map'
+    local nmap = require('utils.map').nmap
+    nmap('ga', dialmap.inc_normal, 'increment')
+    nmap('gs', dialmap.dec_normal, 'decrement')
+  end,
 }
