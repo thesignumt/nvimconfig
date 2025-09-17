763f8c9fe67881939b7ef627b95b626abcad14f6
thesignumt <m96153045@gmail.com>
2025-09-17 16:19:24 +0100

    chore: format

--- lua/plugins/registers.lua
+++ lua/plugins/registers.lua
@@ -1,13 +1,14 @@
 return {
-    'tversteeg/registers.nvim',
-    cmd = "Registers",
-    config = true,
-    keys = {
-        { "\"",    mode = { "n", "v" } },
-        { "<C-R>", mode = "i" },
-    },
-    name = "registers",
-    init = function()
-        require("registers").setup({})
-    end
+  'tversteeg/registers.nvim',
+  cmd = 'Registers',
+  config = true,
+  keys = {
+    { '"', mode = { 'n', 'v' } },
+    { '<C-R>', mode = 'i' },
+  },
+  name = 'registers',
+  init = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('registers').setup {}
+  end,
 }
