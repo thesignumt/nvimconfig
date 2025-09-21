0b21339aaf27421b5f79c170eecb956e5b8fbd22
thesignumt <m96153045@gmail.com>
2025-09-21 11:22:53 +0100

    test: minimal gitsigns

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -36,49 +36,49 @@ require('lazy').setup({
   { -- Git signs
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map('n', ']c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { ']c', bang = true }
-          else
-            gitsigns.nav_hunk 'next'
-          end
-        end, { desc = 'next git change' })
-
-        map('n', '[c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { '[c', bang = true }
-          else
-            gitsigns.nav_hunk 'prev'
-          end
-        end, { desc = 'prev git change' })
-
-        map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>gr', gitsigns.reset_hunk, { desc = 'git reset hunk' })
-      end,
+      -- signs = {
+      --   add = { text = '+' },
+      --   change = { text = '~' },
+      --   delete = { text = '_' },
+      --   topdelete = { text = '‾' },
+      --   changedelete = { text = '~' },
+      -- },
+      -- on_attach = function(bufnr)
+      --   local gitsigns = require 'gitsigns'
+      --
+      --   local function map(mode, l, r, opts)
+      --     opts = opts or {}
+      --     opts.buffer = bufnr
+      --     vim.keymap.set(mode, l, r, opts)
+      --   end
+      --
+      --   -- Navigation
+      --   map('n', ']c', function()
+      --     if vim.wo.diff then
+      --       vim.cmd.normal { ']c', bang = true }
+      --     else
+      --       gitsigns.nav_hunk 'next'
+      --     end
+      --   end, { desc = 'next git change' })
+      --
+      --   map('n', '[c', function()
+      --     if vim.wo.diff then
+      --       vim.cmd.normal { '[c', bang = true }
+      --     else
+      --       gitsigns.nav_hunk 'prev'
+      --     end
+      --   end, { desc = 'prev git change' })
+      --
+      --   map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'git stage hunk' })
+      --   map('n', '<leader>gr', gitsigns.reset_hunk, { desc = 'git reset hunk' })
+      -- end,
     },
-    config = function(_, opts)
-      local gitsigns = require 'gitsigns'
-      gitsigns.setup(opts)
-      nmap('<leader>gp', gitsigns.preview_hunk, 'preview hunk')
-      nmap('<leader>gt', gitsigns.toggle_current_line_blame, 'toggle blame')
-    end,
+    -- config = function(_, opts)
+    --   local gitsigns = require 'gitsigns'
+    --   gitsigns.setup(opts)
+    --   nmap('<leader>gp', gitsigns.preview_hunk, 'preview hunk')
+    --   nmap('<leader>gt', gitsigns.toggle_current_line_blame, 'toggle blame')
+    -- end,
   },
 
   { -- Which-key for keybind hints
