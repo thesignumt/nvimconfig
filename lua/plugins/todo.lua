214b4a59fad21960ef267829f8a7cd0b9911a01f
thesignumt <m96153045@gmail.com>
2025-09-21 12:31:00 +0100

    feat(todo): remaps for Dooing

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -4,5 +4,9 @@ return {
     require('dooing').setup {
       quick_keys = false,
     }
+    local nmap = require('utils.map').nmap
+
+    nmap('<leader>n', ':Dooing<cr>', 'todo')
+    nmap('<leader>N', ':DooingLocal<cr>', 'todo local')
   end,
 }
