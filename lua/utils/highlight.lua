c8c248e790be3d6c0b828014fbf4df5012a842b7
thesignumt <m96153045@gmail.com>
2025-09-22 18:58:55 +0100

    revert(highlight): removed spell highlighting because it doesn't work

--- lua/utils/highlight.lua
+++ lua/utils/highlight.lua
@@ -5,12 +5,6 @@ function M.setup()
     -- border
     vim.api.nvim_set_hl(0, 'ThesignumtBd', { fg = '#20f6e2', bg = 'NONE' })
     vim.api.nvim_set_hl(0, 'ThesignumtDocBd', { fg = '#20f6e2', bg = 'NONE' })
-
-    -- spell
-    vim.api.nvim_set_hl(0, 'SpellBad', { undercurl = true, sp = '#ff0000' }) -- red squiggly
-    vim.api.nvim_set_hl(0, 'SpellCap', { undercurl = true, sp = '#ffa500' }) -- orange squiggly for capitalized
-    vim.api.nvim_set_hl(0, 'SpellLocal', { undercurl = true, sp = '#1e90ff' }) -- blue squiggly
-    vim.api.nvim_set_hl(0, 'SpellRare', { undercurl = true, sp = '#800080' }) -- purple squiggly
   end
 
   apply()
