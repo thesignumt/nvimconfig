b824799abe7a62daf733504df9aa86bb8003774f
thesignumt <m96153045@gmail.com>
2025-09-22 18:50:32 +0100

    feat(highlight): add spell highlights and changed border highlight group names

--- lua/utils/highlight.lua
+++ lua/utils/highlight.lua
@@ -2,14 +2,19 @@ local M = {}
 
 function M.setup()
   local function apply()
-    vim.api.nvim_set_hl(0, 'CmpBorder', { fg = '#20f6e2', bg = 'NONE' })
-    vim.api.nvim_set_hl(0, 'CmpDocBorder', { fg = '#20f6e2', bg = 'NONE' })
+    -- border
+    vim.api.nvim_set_hl(0, 'ThesignumtBd', { fg = '#20f6e2', bg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'ThesignumtDocBd', { fg = '#20f6e2', bg = 'NONE' })
+
+    -- spell
+    vim.api.nvim_set_hl(0, 'SpellBad', { undercurl = true, sp = '#ff0000' }) -- red squiggly
+    vim.api.nvim_set_hl(0, 'SpellCap', { undercurl = true, sp = '#ffa500' }) -- orange squiggly for capitalized
+    vim.api.nvim_set_hl(0, 'SpellLocal', { undercurl = true, sp = '#1e90ff' }) -- blue squiggly
+    vim.api.nvim_set_hl(0, 'SpellRare', { undercurl = true, sp = '#800080' }) -- purple squiggly
   end
 
-  -- apply once on startup
   apply()
 
-  -- reapply every time colorscheme changes
   vim.api.nvim_create_autocmd('ColorScheme', {
     callback = apply,
   })
