129ac7d699a282b978a5bd2a985d4667bc7e81cf
thesignumt <m96153045@gmail.com>
2025-09-22 23:48:34 +0100

    feat(genor)

--- lua/core/genor.lua
+++ lua/core/genor.lua
@@ -1,51 +1,56 @@
-local function sengen()
-  local words = {
-    'lorem',
-    'ipsum',
-    'dolor',
-    'sit',
-    'amet',
-    'consectetur',
-    'adipiscing',
-    'elit',
-    'sed',
-    'do',
-    'eiusmod',
-    'tempor',
-    'incididunt',
-    'ut',
-    'labore',
-    'et',
-    'dolore',
-    'magna',
-    'aliqua',
-    'enim',
-    'ad',
-    'minim',
-    'veniam',
-    'quis',
-    'nostrud',
-    'exercitation',
-    'ullamco',
-    'laboris',
-    'nisi',
-    'ut',
-    'aliquip',
-    'ex',
-    'ea',
-    'commodo',
-    'consequat',
-  }
+local SenGen = {}
+
+SenGen.words = {
+  'lorem',
+  'ipsum',
+  'dolor',
+  'sit',
+  'amet',
+  'consectetur',
+  'adipiscing',
+  'elit',
+  'sed',
+  'do',
+  'eiusmod',
+  'tempor',
+  'incididunt',
+  'ut',
+  'labore',
+  'et',
+  'dolore',
+  'magna',
+  'aliqua',
+  'enim',
+  'ad',
+  'minim',
+  'veniam',
+  'quis',
+  'nostrud',
+  'exercitation',
+  'ullamco',
+  'laboris',
+  'nisi',
+  'ut',
+  'aliquip',
+  'ex',
+  'ea',
+  'commodo',
+  'consequat',
+}
+
+function SenGen:get()
   local sentence = {}
   for _ = 1, math.random(5, 15) do
-    table.insert(sentence, words[math.random(#words)])
+    table.insert(sentence, self.words[math.random(#self.words)])
   end
-  sentence[1] = sentence[1]:gsub('^%l', string.upper) -- Capitalize the first word
+
+  sentence[1] = sentence[1]:gsub('^%l', string.upper)
   return table.concat(sentence, ' ') .. '.'
 end
 
+-- Create a Neovim user command
 vim.api.nvim_create_user_command('SenGen', function()
-  local sentence = sengen()
+  local sentence = SenGen:get()
   vim.fn.setreg('+', sentence)
   print 'sengen yanked'
 end, {})
