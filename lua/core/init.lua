abc9bc2fd82d1c4df10893cad6a85ceae502829b
thesignumt <m96153045@gmail.com>
2025-09-24 20:37:17 +0100

    feat(cursors): cycle through a list of guicursors

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -13,8 +13,12 @@ require 'core.typst'
 -- [                      utils setup                      ]
 -- +-------------------------------------------------------+
 local nmap = require('utils.map').nmap
+local inst = require('utils.f').inst_fn
 
 require('utils.highlight').setup()
 
 local pick = require 'utils.pick'
 nmap('<leader>sc', pick.colorscheme, 'colorscheme')
+
+local c = require('utils.cursor').new()
+nmap('<F2>', inst(c) { fn = 'toggle' })
---
+++ lua/utils/cursor.lua
@@ -0,0 +1,93 @@
+---@class Cursor
+---@field cursors string[]
+---@field index integer
+---@field default_index integer
+---@field default_file string
+local Cursor = {}
+Cursor.__index = Cursor
+
+---@param cursors string[]? List of cursor styles
+---@return Cursor
+function Cursor.new(cursors)
+  local self = setmetatable({
+    cursors = cursors or { 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20', '' },
+    index = 1,
+    default_file = vim.fn.stdpath 'data' .. '/cursor_default', -- persistent file
+    default_index = 1,
+  }, Cursor)
+
+  -- Load default index from file
+  self:_load_default()
+  self:set(self.default_index, { show = false })
+
+  return self
+end
+
+---Set the cursor to a specific index
+---@param index integer
+---@param opts {show: boolean, setdefault: boolean}? Optional: show print and set as default
+function Cursor:set(index, opts)
+  if index < 1 or index > #self.cursors then
+    error(('Index %d out of range (1-%d)'):format(index, #self.cursors))
+  end
+
+  self.index = index
+  local current = self.cursors[self.index]
+  vim.opt.guicursor = current
+
+  opts = opts or {}
+  local show = opts.show ~= nil and opts.show or true
+  if show then
+    print(("%d -> '%s'"):format(self.index, current))
+  end
+
+  -- Set as default if requested
+  if opts.setdefault then
+    self.default_index = self.index
+    self:_save_default()
+  end
+end
+
+---Toggle to the next cursor in the list (cycles automatically)
+---@param opts {show: boolean, setdefault: boolean}? Optional: show print and set as default
+function Cursor:toggle(opts)
+  local next_index = (self.index % #self.cursors) + 1
+  self:set(next_index, opts)
+end
+
+---Set the default cursor index for future sessions
+---@param index integer
+function Cursor:_setdefault(index)
+  if index < 1 or index > #self.cursors then
+    error(('Index %d out of range (1-%d)'):format(index, #self.cursors))
+  end
+  self.default_index = index
+  self:_save_default()
+  -- print(('Default cursor set to index %d'):format(index))
+end
+
+---Save the default index to file
+function Cursor:_save_default()
+  local f = io.open(self.default_file, 'w')
+  if not f then
+    vim.notify('Failed to write cursor default file', vim.log.levels.ERROR)
+    return
+  end
+  f:write(tostring(self.default_index))
+  f:close()
+end
+
+---Load the default index from file
+function Cursor:_load_default()
+  local f = io.open(self.default_file, 'r')
+  if f then
+    local content = f:read '*l'
+    f:close()
+    local idx = tonumber(content)
+    if idx and idx >= 1 and idx <= #self.cursors then
+      self.default_index = idx
+    end
+  end
+end
+
+return Cursor
