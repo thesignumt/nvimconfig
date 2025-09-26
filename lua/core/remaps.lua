48ba56e7aa20ba5a40fba38e4de5983b0953d57f
thesignumt <tcrrxzz@gmail.com>
2025-09-26 17:35:25 +0100

    refactor(remaps): <leader>o rhs

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -49,7 +49,7 @@ nmap(
 nmap('<leader>q', ':q<cr>')
 nmap('<leader>Q', ':qa!<cr>', 'quit neovim')
 nmap('<leader>w', ':write<cr>')
-nmap('<leader>o', ':update<cr>:source<cr>')
+nmap('<leader>o', ':up<cr>:so<cr>', 'source')
 -- open RC files
 nmap('<leader>v', ':e $MYVIMRC<cr>')
 nmap('<leader>ew', ':e ~/.config/wezterm/wezterm.lua<cr>')
