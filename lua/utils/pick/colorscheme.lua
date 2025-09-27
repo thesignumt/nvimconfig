27bf7199374ddc05a7e0087d9ebcf4496ca525f9
thesignumt <tcrrxzz@gmail.com>
2025-09-27 15:17:29 +0100

    feat(pick/colorscheme): added callback for themes

--- lua/utils/pick/colorscheme.lua
+++ lua/utils/pick/colorscheme.lua
@@ -1,18 +1,38 @@
 ---Set the colorscheme by parameter or pick using Telescope
 ---@param theme string?
 local function colorscheme(theme)
-  local themes = {
-    'tokyonight-night',
-    'rose-pine-moon',
-    'vague',
+  ---@class Theme
+  ---@field name string
+  ---@field callback? fun(): nil
+
+  ---@type Theme[]
+  local Themes = {
+    {
+      name = 'tokyonight-night',
+    },
+    {
+      name = 'rose-pine-moon',
+    },
+    {
+      name = 'vague',
+    },
   }
 
   vim.validate('theme', theme, 'string', true)
 
+  local function apply_theme(entry)
+    vim.cmd.colorscheme(entry.name)
+    if entry.callback then
+      entry.callback()
+    else
+      vim.notify('colorscheme: ' .. entry.name, vim.log.levels.INFO)
+    end
+  end
+
   if theme then
-    for _, v in ipairs(themes) do
-      if v == theme then
-        return vim.cmd.colorscheme(v)
+    for _, v in ipairs(Themes) do
+      if v.name == theme then
+        return apply_theme(v)
       end
     end
     vim.notify(
@@ -31,14 +51,23 @@ local function colorscheme(theme)
   pickers
     .new({}, {
       prompt_title = 'Select Colorscheme',
-      finder = finders.new_table { results = themes },
+      finder = finders.new_table {
+        results = Themes,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = entry.name,
+            ordinal = entry.name,
+          }
+        end,
+      },
       sorter = sorters(),
       attach_mappings = function(_, map)
         local function select_colorscheme(prompt_bufnr)
           local entry = action_state.get_selected_entry()
           actions.close(prompt_bufnr)
           if entry then
-            vim.cmd.colorscheme(entry[1])
+            apply_theme(entry.value)
           end
         end
 
