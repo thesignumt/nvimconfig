e2673f2afeefc03f12f76ccf330857b3dc31cc36
thesignumt <tcrrxzz@gmail.com>
2025-09-28 12:28:53 +0100

    doc(undotree): remap description

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
   'mbbill/undotree',
 
   config = function()
-    require('utils.map').nmap('<leader>u', vim.cmd.UndotreeToggle)
+    require('utils.map').nmap('<leader>u', vim.cmd.UndotreeToggle, 'undotree')
   end,
 }
