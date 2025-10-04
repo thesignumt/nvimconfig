5fbee64d85c13eb5abd7a4088614d77a4f6a38eb
thesignumt <tcrrxzz@gmail.com>
2025-10-04 09:26:15 +0100

    enable codecompanion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,21 +1,21 @@
 return {
   { 'github/copilot.vim' },
-  -- {
-  --   'olimorris/codecompanion.nvim',
-  --   opts = {
-  --     log_level = 'DEBUG',
-  --     strategies = {
-  --       chat = {
-  --         adapter = 'copilot',
-  --       },
-  --       inline = {
-  --         adapter = 'copilot',
-  --       },
-  --     },
-  --   },
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim',
-  --     'nvim-treesitter/nvim-treesitter',
-  --   },
-  -- },
+  {
+    'olimorris/codecompanion.nvim',
+    opts = {
+      log_level = 'DEBUG',
+      strategies = {
+        chat = {
+          adapter = 'copilot',
+        },
+        inline = {
+          adapter = 'copilot',
+        },
+      },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
 }
