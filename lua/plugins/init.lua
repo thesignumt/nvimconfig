c44a2640a747148a539718f148eb9e987bbe1c79
thesignumt <tcrrxzz@gmail.com>
2025-10-15 16:38:22 +0100

    fix(pear): pear used correct functio

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -122,9 +122,10 @@ return {
     'sylvanfranklin/pear',
     config = function()
       local pear = require 'pear'
-      vim.keymap.set('n', '<leader>b', function()
-        pear.jump_pairs()
-      end, { silent = true })
+      local nmap = require('utils.map').nmap
+      nmap('<leader>b', function()
+        pear.jump_pair()
+      end)
     end,
   },
 }
