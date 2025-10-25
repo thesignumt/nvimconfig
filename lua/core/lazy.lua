d3f27712098471172fbee8582a3849aa2744b583
thesignumt <tcrrxzz@gmail.com>
2025-10-25 19:04:04 +0100

    chore(lazy): remove unused plugin

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -38,7 +38,7 @@ require('lazy').setup({
     'echasnovski/mini.nvim',
     config = function()
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
+      -- require('mini.surround').setup() -- removed since i am used to using nvim-surround
       require('mini.icons').setup()
       require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
     end,
