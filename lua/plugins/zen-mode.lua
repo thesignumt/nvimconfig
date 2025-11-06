ddfea26faa1ba570161e76e1871699337d469d13
thesignumt <tcrrxzz@gmail.com>
2025-11-06 16:01:45 +0000

    refactor(zen): inline require

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,9 +1,7 @@
 return {
   'folke/zen-mode.nvim',
   config = function()
-    local Zen = require 'utils.zen'
-    local zen = Zen.new()
-
+    local zen = require('utils.zen').new()
     local nmap = require('utils.map').nmap
 
     nmap('<leader>zz', function()
