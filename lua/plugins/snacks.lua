419d13d1c9ce1b5826b0a541adb986dec5a0fef8
thesignumt <tcrrxzz@gmail.com>
2025-11-07 08:14:35 +0000

    feat(snacks): github cli

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,14 +2,51 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  opts = {
-    bigfile = { enabled = true },
-    bufdelete = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+  opts = vim.tbl_map(function()
+    return { enabled = true }
+  end, {
+    'bigfile',
+    'bufdelete',
+    'gh',
+    'input',
+    'notifier',
+    'quickfile',
+    'scope',
+    'statuscolumn',
+    'words',
+  }),
+  config = function(_, opts)
+    opts.picker = { sources = { gh_issue = {}, gh_pr = {} } }
+    require('snacks').setup(opts)
+  end,
+  keys = {
+    {
+      '<leader>gi',
+      function()
+        require('snacks').picker.gh_issue()
+      end,
+      desc = 'GitHub Issues (open)',
+    },
+    {
+      '<leader>gI',
+      function()
+        require('snacks').picker.gh_issue { state = 'all' }
+      end,
+      desc = 'GitHub Issues (all)',
+    },
+    {
+      '<leader>gp',
+      function()
+        require('snacks').picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        require('snacks').picker.gh_pr { state = 'all' }
+      end,
+      desc = 'GitHub Pull Requests (all)',
+    },
   },
 }
