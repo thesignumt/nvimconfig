ef266fdfd4939109e45cb694652a2c3a61011c03
thesignumt <tcrrxzz@gmail.com>
2026-02-02 22:04:00 +0000

    chore(mini): remove mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     'echasnovski/mini.nvim',
     config = function()
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
       require('mini.icons').setup()
       require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
     end,
