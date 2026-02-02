b4f6221b4ec0398f04baf93484cde612722b0cb2
thesignumt <tcrrxzz@gmail.com>
2026-02-02 20:38:00 +0000

    chore(term): simplify shell cmd flag

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,5 @@
 vim.o.shell = 'pwsh'
-vim.o.shellcmdflag =
-  '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command'
 vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.o.shellpipe = '2>&1 | Tee-Object -FilePath %s; exit $LastExitCode'
 vim.o.shellquote = ''
