d3a0c2b79821480a9cfccc6ce9dbf62e9d64ae84
thesignumt <tcrrxzz@gmail.com>
2026-02-24 21:22:56 +0000

    fix(lsp): fixed lua_ls startup time

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -36,7 +36,9 @@ vim.lsp.config('lua_ls', {
         globals = { 'vim' },
       },
       workspace = {
-        library = vim.api.nvim_get_runtime_file('', true),
+        library = {
+          vim.env.VIMRUNTIME,
+        },
         checkThirdParty = false,
       },
       telemetry = { enable = false },
